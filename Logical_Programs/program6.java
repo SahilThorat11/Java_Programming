f3a3ffa02478779a1a67ebb19cffdae9f0a11fd8
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-11-07 12:01:26 +0530

    Warmup codes

---
+++ Logical_Programs/program6.java
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void Display()                // It should be int main
+{
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
