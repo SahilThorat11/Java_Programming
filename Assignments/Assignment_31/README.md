a03b5bcc8e049447471f421553bd9ad463d10a57
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-11-23 12:56:00 +0530

    Problems on pattern printing assignment solutions

---
+++ Assignments/Assignment_31/README.md
@@ -0,0 +1,58 @@
+# Assignment 31 :
+
+This folder contains problems related to Pattern printing in which input is taken from user as a Integer, problems are:-
+
+    1. Accept number from user and display below pattern,
+
+        //  Input  : iRow = 5   iCol = 5
+        //
+        //  Output :    *
+        //              *       *
+        //              *       *       *
+        //              *       *       *       *
+        
+    2. Accept number from user and display below pattern,
+
+        //  Input  : iRow = 4   iCol = 4
+        //
+        //  Output :    *       *       *       *
+        //              *       *       *
+        //              *       *
+        //              *
+
+    3. Accept number from user and display below pattern,
+
+        //  Input  : iRow = 5   iCol = 5
+        //
+        //  Output :    $       *       *       *       *   
+        //              #       $       *       *       *
+        //              #       #       $       *       *
+        //              #       #       #       $       *
+        //              #       #       #       #       $
+
+    4. Accept number from user and display below pattern,
+
+        //  Input  : iRow = 5   iCol = 5
+        //
+        //  Output :    *       *       *       *       *
+        //              *       *                       *
+        //              *               *               *
+        //              *                       *       *
+        //              *       *       *       *       *
+
+    5. Accept number from user and display below pattern,
+
+        //  Input  : iRow = 4   iCol = 4
+        //
+        //  Output :    1       2       3       4
+        //                      2       3       4
+        //                              3       4
+        //                                      4
+
+Commands used to execute these codes are:-
+
+javac filename.java
+
+java filename
+
+└── Assignment └── Assignment_31 ├── Program31_1.java ├── Program31_2.java ├── Program31_3.java ├── Program32_4.java ├── Program35_5.java └── README.md
---
+++ Assignments/Assignment_31/program31_5.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description   : Accepts a number from user and displays first # symbol then 
+//                  number and then * Symbol in Accending order.     
+//  Input         : Integer, Integer
+//  Output        : Void
+//  Author        : Sahil Rajaram Thorat
+//  Date          : 23/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol ; j++)
+            {
+                if(i <= j) 
+                {
+                    System.out.print(j + "\t");
+                }
+                else
+                {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+    }
+} // End of Pattern class
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+////////////////////////////////////////////////////////////////////////////////
+  
+class program31_5
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+} // End of main class
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Testcases succesfully handle by the application
+//
+//  Input  : iRow = 5   iCol = 5
+//
+//  Output :    1       2       3       4
+//                      2       3       4
+//                              3       4
+//                                      4
+//
+////////////////////////////////////////////////////////////////////////////////
