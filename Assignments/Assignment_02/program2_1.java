f347e6b1f481c06eb1745303116dbdd1f08426de
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2025-12-08 13:01:36 +0530

    Add Assignment_02 solutions

--- Assignments/Assignment_02/program2_1.java
+++ Assignments/Assignment_02/program2_1.java
@@ -1,30 +1,11 @@
 import java.util.Scanner;
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Class Name     : Display
-//  Description    : Contains method to print symbol in reverse order
-//  Author         : Sahil Rajaram Thorat
-//  Date           : 07/12/2025
-//
-////////////////////////////////////////////////////////////////////////////////
-
 class Demo
 {
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    //  Function Name : Display
-    //  Description   : Use to print 1 to 5 in reverse order.
-    //  Input         :  
-    //  Output        : void
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
     void Display(int iNo)
     {
         int iCnt = 0;
 
-        // Updater
         if (iNo < 0)
         {
             iNo = -iNo;
@@ -35,27 +16,11 @@ class Demo
             System.out.println("*\t");
             iCnt++;
         }
-    } // End of Accept
+    } 
 }
-
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Class Name     : Program1_3
-//  Description    : Entry point for the application
-//  Author         : Sahil Rajaram Thorat
-//  Date           : 17/10/2025
-//
-////////////////////////////////////////////////////////////////////////////////
 
 class program2_1
 {
-    ////////////////////////////////////////////////////////////////////////////
-    //
-    //  Function Name : main
-    //  Description   : Entry point function which takes input from user
-    //
-    ////////////////////////////////////////////////////////////////////////////
-
     public static void main(String A[])
     {
         int iValue = 0;
@@ -66,18 +31,6 @@ class program2_1
 
         Demo dobj = new Demo();
         dobj.Display(iValue);
-    } // End of main
+    }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Testcases successfully handled by the application
-//
-//  Input  : 5               Input  : 2
-//  Output : *               Output : *
-//           *                        *
-//           *
-//           *
-//           *
-//
-////////////////////////////////////////////////////////////////////////////////
