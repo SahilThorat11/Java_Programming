597f33b87e1c2316e148d074692bdb4fd1d74eda
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-29 13:32:55 +0530

    Add file

---
+++ Assignments/Assignment_58/Program_05.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Program_05 
+{
+    public static void main(String A[]) throws Exception 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter directory name: ");
+        String dirName = sobj.nextLine();
+
+        File dir = new File(dirName);
+        FileWriter fwobj = new FileWriter("Marvellous.txt");
+
+        if (dir.exists() && dir.isDirectory()) 
+        {
+            File[] files = dir.listFiles();
+
+            for (File file : files) 
+            {
+                if (file.isFile()) 
+                {
+                    fwobj.write("File Name: " + file.getName() + "\n");
+                    fwobj.write("File Size: " + file.length() + " bytes\n");
+
+                    FileReader frobj = new FileReader(file);
+                    int ch;
+                    while ((ch = frobj.read()) != -1) 
+                    {
+                        fwobj.write(ch);
+                    }
+                    fwobj.write("\n----------------------------------\n");
+                    frobj.close();
+                }
+            }
+            System.out.println("File name, size and data written successfully.");
+        } 
+        else 
+        {
+            System.out.println("Invalid directory.");
+        }
+
+        fwobj.close();
+    }
+}
