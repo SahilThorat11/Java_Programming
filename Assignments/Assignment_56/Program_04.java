bb644d951e1212ba1685891204774a26106e823d
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-29 13:04:41 +0530

    Program to creat new file

---
+++ Assignments/Assignment_56/Program_04.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Program_04 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter file name: ");
+        String fileName = sc.nextLine();
+
+        File file = new File(fileName);
+
+        if (file.createNewFile()) 
+        {
+            System.out.println("File created successfully.");
+        } 
+        else
+        {
+            System.out.println("File already exists.");
+        }
+    }
+}
