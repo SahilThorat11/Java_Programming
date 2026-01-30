8668060d031732ff7bb2a2cec58ed1fb21b193a3
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-30 12:44:20 +0530

    Display file name along with its absolute path

---
+++ Assignments/Assignment_59/Program_05.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Program_05
+{
+    public static void main(String A[]) throws Exception
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the name of Directory : ");
+        String DirectoryName = sobj.nextLine();
+
+        File fobj = new File(DirectoryName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            System.out.println("Directory is present.");
+
+            File fArr[] = fobj.listFiles();
+
+            System.out.println("Number of files in the Directorys are : " + fArr.length);
+
+            for(int i = 0; i < fArr.length; i++)
+            {
+                System.out.println("File Name : " + fArr[i].getName() + "\tFile Path : " + fArr[i].getAbsolutePath());
+            }
+        }
+        else
+        {
+            System.out.println("There is no such Directory.");
+        }
+        
+        sobj.close();
+    }
+}
