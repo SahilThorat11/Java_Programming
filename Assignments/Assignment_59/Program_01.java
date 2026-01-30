d16c6c696eda155385386e51322cf5e05507e5b1
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-30 12:31:16 +0530

    List all files is directory

---
+++ Assignments/Assignment_59/Program_01.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class Program_01
+{
+    public static void main(String A[]) throws Exception
+    {
+        int iRet = 0;
+        byte Buffer[] = new byte[1024];
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the name of Directory : ");
+        String DirectoryName = sobj.nextLine();
+
+        File fobj = new File(DirectoryName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            System.out.println("Directory is present.");
+
+            File fArr[] = fobj.listFiles();
+
+            System.out.println("Number of files in the Directorys are : " + fArr.length);
+
+            for(int i = 0; i < fArr.length; i++)
+            {
+                System.out.println("File Name : " + fArr[i].getName() + "\tFile Size : " + fArr[i].length() + "bytes");
+            }
+        }
+        else
+        {
+            System.out.println("There is no such Directory.");
+        }
+        
+        sobj.close();
+    }
+}
