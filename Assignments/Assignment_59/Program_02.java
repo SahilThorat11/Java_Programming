b3ed22398a657e7c160bc45cb8080ee4e268f8be
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-30 12:39:37 +0530

    List only regular files

--- Assignments/Assignment_59/Program_02.java
+++ Assignments/Assignment_59/Program_02.java
@@ -5,9 +5,6 @@ class Program_02
 {
     public static void main(String A[]) throws Exception
     {
-        int iRet = 0;
-        byte Buffer[] = new byte[1024];
-
         Scanner sobj = new Scanner(System.in);
 
         System.out.print("Enter the name of Directory : ");
