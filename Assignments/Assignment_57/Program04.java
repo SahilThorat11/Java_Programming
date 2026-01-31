1134cc9476a23d681f67c1e3b4c20aa3764031e2
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-31 20:35:20 +0530

    Calculate checkSum of file and display

--- Assignments/Assignment_57/Program04.java
+++ Assignments/Assignment_57/Program04.java
@@ -1,26 +1,31 @@
-import java.io.FileInputStream;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 import java.util.zip.CRC32;
 
 class Program_04 
 {
     public static void main(String A[]) throws Exception 
     {
-        Scanner sobj = new Scanner(System.in);
+        int data = 0; 
+        Scanner sobj = null;
+        String fileName = null;
+        FileInputStream fiobj = null;
+        CRC32 checksum = null;
+        
+        sobj = new Scanner(System.in);
 
         System.out.print("Enter file name: ");
-        String fileName = sobj.nextLine();
+        fileName = sobj.nextLine();
 
-        FileInputStream fisobj = new FileInputStream(fileName);
-        CRC32 checksum = new CRC32();
+        fiobj = new FileInputStream(fileName);
+        checksum = new CRC32();
 
-        int data;
-        while ((data = fisobj.read()) != -1) 
+        while ((data = fiobj.read()) != -1) 
         {
             checksum.update(data);
         }
 
-        fisobj.close();
+        fiobj.close();
 
         System.out.println("Checksum value: " + checksum.getValue());
     }
