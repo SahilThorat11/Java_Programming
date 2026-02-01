25d628a8af4757f644068d39f697ea12b64fa7aa
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-01 20:21:05 +0530

    Count total number of files and folders from directory

--- Assignments/Assignment_59/Program_04.java
+++ Assignments/Assignment_59/Program_04.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class Program_04
 {
-    public static void main(String A[]) throws Exception
+    public static void main(String A[])
     {
         int iCountFolder = 0;
         int iCountFiles = 0;
@@ -12,41 +12,46 @@ class Program_04
         File fobj = null;
         File fArr[] = null;
 
-        sobj = new Scanner(System.in);
-
-        System.out.print("Enter the name of Directory : ");
-        DirName = sobj.nextLine();
-
-        fobj = new File(DirName);
-
-        if((fobj.exists()) && (fobj.isDirectory()))
+        try
         {
-            System.out.println("Directory is present.");
+            sobj = new Scanner(System.in);
 
-            fArr = fobj.listFiles();
+            System.out.print("Enter the name of Directory : ");
+            DirName = sobj.nextLine();
 
-            System.out.println("Number of files in the Directorys are : " + fArr.length);
+            fobj = new File(DirName);
 
-            for(int i = 0; i < fArr.length; i++)
+            if((fobj.exists()) && (fobj.isDirectory()))
             {
-                if(fArr[i].isDirectory())
-                {
-                    iCountFolder++;
-                }
-                else 
-                {
-                    iCountFiles++;
-                }
-            }
+                System.out.println("Directory is present.");
 
-            System.out.println("Number of folders in the Directorys are : " + iCountFolder);
-            System.out.println("Number of files in the Directorys are : " + iCountFiles);
+                fArr = fobj.listFiles();
+
+                System.out.println("Number of files in the Directorys are : " + fArr.length);
+
+                for(int i = 0; i < fArr.length; i++)
+                {
+                    if(fArr[i].isDirectory())
+                    {
+                        iCountFolder++;
+                    }
+                    else 
+                    {
+                        iCountFiles++;
+                    }
+                }
+
+                System.out.println("Number of folders in the Directorys are : " + iCountFolder);
+                System.out.println("Number of files in the Directorys are : " + iCountFiles);
+            }
+            else
+            {
+                System.out.println("There is no such Directory.");
+            }
+            
+            sobj.close();
         }
-        else
-        {
-            System.out.println("There is no such Directory.");
-        }
-        
-        sobj.close();
+        catch(Exception eobj)
+        {}
     }
 }
