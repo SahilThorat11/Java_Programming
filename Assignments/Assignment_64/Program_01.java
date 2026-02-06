d9fbbc20c73ae4027e92cf782aa3426538b6748e
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-06 11:13:17 +0530

    Program which checks whether 15th bit is On or Off

---
+++ Assignments/Assignment_64/Program_01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Program_01
+{
+    public static void main(String A[])
+    {
+        int iNo = 0, iMask = 0, iResult = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number  : ");
+        iNo = sobj.nextInt();
+
+        iMask = 0x00004000;
+
+        iResult = iNo & iMask;
+
+        if(iResult == iMask)
+        {
+            System.out.println("15th bit is ON");
+        }
+        else
+        {
+            System.out.println("15th bit is OFF");
+        }
+    }
+}
