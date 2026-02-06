56048bd54924d4bdf06511d008f864bacc1315f4
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-06 12:59:11 +0530

    Program to On the bit of Nth position and return modified number

---
+++ Assignments/Assignment_66/Program_03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Program_03
+{
+    public static void main(String A[])
+    {
+        int iNo = 0, iMask = 0x00000001, iResult = 0, iPos = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number  : ");
+        iNo = sobj.nextInt();
+
+        System.out.print("Enter the Position  : ");
+        iPos = sobj.nextInt();
+
+        iMask = iMask << (iPos - 1);
+
+        iResult = iNo | iMask;
+
+        System.out.println("Modified number is : " + iResult);
+        
+    }
+}
