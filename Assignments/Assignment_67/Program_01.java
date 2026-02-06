ff4a3af8159ec2cd0deff34394d6a1b45ff4131c
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-06 13:13:05 +0530

    Count number of On bits (1) from given number

---
+++ Assignments/Assignment_67/Program_01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Program_01
+{
+    public static void main(String A[])
+    {
+        int iNo = 0;
+        int iCount = 0;
+        int iMask = 0x00000001;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number  : ");
+        iNo = sobj.nextInt();
+
+        while(iNo != 0)
+        {
+            if((iNo & iMask) == 1)
+            {
+                iCount++;
+            }
+
+            iNo = iNo >> iMask;
+        }
+
+        System.out.println("Number of 1's from given number are : " + iCount);
+    }
+}
