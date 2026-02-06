69e6bd675bb70143222fe70ae5cb6216cd5713e4
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-06 11:52:49 +0530

    Program to off 7th and 10th  bit of number and return modified number

---
+++ Assignments/Assignment_65/Program_02.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Program_02
+{
+    public static void main(String A[])
+    {
+        int iNo = 0, iMask = 0, iResult = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the Number  : ");
+        iNo = sobj.nextInt();
+
+        iMask = 0x00000240;
+    
+        iResult = iNo & (~iMask);
+
+        System.out.println("Modified number is : " + iResult);
+    }
+}
