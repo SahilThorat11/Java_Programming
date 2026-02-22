4da3560dda32a4b44e52fdcda0bc0fea2bbc6369
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-22 19:32:32 +0530

    Fuction to Accept the elements of Matrix

--- Logical_Programs/Problems_On_Matrix/program03.java
+++ Logical_Programs/Problems_On_Matrix/program03.java
@@ -17,13 +17,15 @@ class Matrix
     public void Accept()
     {
         int i = 0, j = 0;
-        
+
         Scanner sobj = new Scanner(System.in);
 
-        System.out.println("Enter the elements of Matix : ");
+        System.out.println("Enter the elements of matrix : ");
 
         for (i = 0; i < iRow; i++)
         {
+            System.out.println("Enter the elements of row : " +(i + 1));
+
             for (j = 0; j < iCol; j++)
             {
                 Arr[i][j] = sobj.nextInt();
@@ -32,15 +34,32 @@ class Matrix
 
         sobj.close();
     }
+
+    public void Display()
+    {
+        int i = 0, j = 0;
+
+        System.out.println("Elements of Matix are : ");
+
+        for (i = 0; i < iRow; i++)
+        {
+            for (j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+    }
 }
 
-class Program01
+class Program789
 {
     public static void main(String A[])
     {
         Matrix mobj = new Matrix(4,4);
-
-        mobj.Accept();
         
+        mobj.Accept();
+        mobj.Display();
     }
 }
