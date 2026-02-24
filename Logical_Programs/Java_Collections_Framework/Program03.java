9fe69b6c465c14dcae14387f17084f887db25d2b
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-24 18:28:47 +0530

    Use of HashSet

---
+++ Logical_Programs/Java_Collections_Framework/Program03.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Program03
+{
+    public static void main(String A[])
+    {
+        HashSet<String> set = new HashSet<> ();
+
+        set.add("Marvellous");
+        set.add("Infosystems");
+        set.add("Marvellous");
+
+        System.out.println(set);
+    }
+}
