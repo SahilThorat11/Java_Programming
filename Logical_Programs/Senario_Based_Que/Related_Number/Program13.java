9635c826371dc2a6847b255c47a78adc8a711ef3
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-06 14:11:43 +0530

    Senario Based Quetions(Numbers to String)

---
+++ Logical_Programs/Senario_Based_Que/Related_Number/Program13.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Program13
+{
+    public static void main(String A[])
+    {
+        int iNo = 5678;
+        String str = Integer.toString(iNo);
+
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
