b336b2db1a6d0c9759df2ff53902d77b50c0ebca
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-07 13:17:06 +0530

    Senario based que (sqrt() fun)

---
+++ Logical_Programs/Senario_Based_Que/Related_Number/Program21.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Program21
+{
+    public static void main(String A[])
+    {
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.sqrt(16));
+        System.out.println(Math.sqrt(12));
+    }
+}
