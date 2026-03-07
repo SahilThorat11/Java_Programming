9091af977bea3f3fa42bf2b1e3b21033044afe15
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-07 13:13:13 +0530

    Senario based que (Reverse number)

---
+++ Logical_Programs/Senario_Based_Que/Related_Number/Program17.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Program17
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in); 
+
+        int iNo = 0, iTemp = 0, iDigit = 0, iRev = 0;
+
+        System.out.println("Enter a number : ");
+        iNo = sobj.nextInt();
+
+        iTemp = iNo;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iRev = iRev * 10 + iDigit;
+            iNo = iNo / 10;
+        }
+
+        if(iTemp == iRev)
+        {
+            System.out.println(iTemp + " is Palindrome number");
+        }
+        else
+        {
+            System.out.println(iTemp + " is Not a Palindrome number");
+        }
+
+        sobj.close();
+    }
+}
