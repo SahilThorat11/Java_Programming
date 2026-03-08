d532268e3862c6524838095b4c5f324814c27e42
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-08 20:45:58 +0530

    Senario based que (String)

---
+++ Logical_Programs/Senario_Based_Que/Related_String/Program03.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Program03
+{
+    public static void main(String A[])
+    {
+        String str = "    hello world    ";
+
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
