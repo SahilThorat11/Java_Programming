7192ed7c29648e5df7cb74c46d9b636f10329bfe
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-10 14:23:08 +0530

    Senario Based(String)

---
+++ Logical_Programs/Senario_Based_Que/Related_String/Program15.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Program15
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string : ");
+
+        String str = null;
+
+        str = sobj.nextLine();
+
+        str = str.trim();
+
+        str = str.replaceAll("\\s+", " ");
+    
+        String Arr[] = str.split(" ");
+
+        System.out.println("Number of words : "+Arr.length);
+    }
+}
