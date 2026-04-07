0d67a29104f5b4444f6bfb57ca262da34ee8913f
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-07 11:06:53 +0530

    Java program calculates x raised to the power y

---
+++ Practice/Program01.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Program01
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int x = 0, y = 0, i = 0, iPow = 0;
+
+        System.out.print("Enter base : ");
+        x = sobj.nextInt();
+
+        System.out.print("Enter power : ");
+        y = sobj.nextInt();
+
+        // Handle undefined case
+        if (x == 0 && y == 0)
+        {
+            System.out.println("0^0 is undefined");
+        }
+        // Handle negative power
+        else if (y < 0)
+        {
+            double result = 1.0;
+            
+            for (i = 1; i <= -y; i++)
+            {
+                result = result * x;
+            }
+        
+            System.out.println("Result : " + (1.0 / result));
+        }
+        else
+        {
+            for (iPow = 1, i = 1; i <= y; i++)
+            {
+                iPow = iPow * x;
+            }
+
+            System.out.println("Result : " + iPow);
+        }
+
+        sobj.close();
+    }
+}
