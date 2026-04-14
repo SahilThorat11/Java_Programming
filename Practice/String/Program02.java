98ff09c0bbd8a4ec1a6037d4915e00678d9cbd68
nehathorat18 <thoratneha18@gmail.com>
2026-04-14 13:36:21 -0100

    Check if a string is a palindrome

---
+++ Practice/String/Program02.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+class Palindrome
+{
+    public static boolean isPalindrome(String str) 
+    {
+        int l = 0, r = str.length() - 1;
+
+        while (l < r)
+        {
+            if (str.charAt(l++) != str.charAt(r--)) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public class Program02
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sobj.nextLine();
+
+        boolean result = Palindrome.isPalindrome(input);
+
+        if(result) 
+        {
+            System.out.println("The string is a palindrome.");
+        }
+        else
+        {
+            System.out.println("The string is NOT a palindrome.");
+        }
+
+        sobj.close();
+    }
+}
