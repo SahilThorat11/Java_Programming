684e1232df2dafb53f29d35c544936ddf8d6b7bf
nehathorat18 <thoratneha18@gmail.com>
2026-04-14 13:35:46 -0100

    Reverse a string without using built-in reverse()

---
+++ Practice/String/Program01.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+class ReverseUtil
+{
+    public static String reverse(String str)
+    {
+        StringBuilder sbobj = new StringBuilder();
+
+        for(int i = str.length() - 1; i >= 0; i--) 
+        {
+            sbobj.append(str.charAt(i));
+        }
+
+        return sbobj.toString();
+    }
+}
+
+class Program01
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sobj.nextLine();
+
+        String reversed = ReverseUtil.reverse(input);
+
+        System.out.println("Reversed String: " + reversed);
+
+        sobj.close();
+    }
+}
