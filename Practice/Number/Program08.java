916642199834e67279d07d64325ede8ee6e6ee12
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-26 15:39:12 +0530

    Swap two numbers without using temprory variable

---
+++ Practice/Number/Program08.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Program08
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sobj.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sobj.nextInt();
+
+        System.out.println("Number Before Swap: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Number After Swap: a = " + a + ", b = " + b);
+
+        sobj.close();
+    }
+}
