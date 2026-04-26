dc320cb62cc429fca85cf73a6f819daba1b5435b
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-26 15:25:26 +0530

    Check number is prime or not using 'Math.sqrt()' method

---
+++ Practice/Number/Program06.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Program06
+{
+    public static void main(String A[]) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int iNo = sobj.nextInt();
+
+        boolean isPrime = true;
+
+        if (iNo <= 1) 
+        {
+            isPrime = false;
+        } 
+        else 
+        {
+            for (int i = 2; i <= Math.sqrt(iNo); i++) 
+            {
+                if (iNo % i == 0) 
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if(isPrime == true)
+        {
+            System.out.println(iNo + "  is Prime number");
+        }
+        else
+        {
+            System.out.println(iNo + " is Not a prime number");
+        }
+    }
+}
