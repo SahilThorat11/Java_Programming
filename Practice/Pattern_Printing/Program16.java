ecfc616a7bab4affcf8098b7a7781f39c083daa0
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-28 15:28:20 +0530

    Pattern Printing

---
+++ Practice/Pattern_Printing/Program16.java
@@ -0,0 +1,54 @@
+/*
+    iRow : 6
+    iCol : 6
+
+    *   *   *   *   *   *
+    *               *   *
+    *           *       *
+    *       *           *
+    *   *               *
+    *   *   *   *   *   *
+
+*/
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        for(int i = 1; i <= iRow; i++)
+        {
+            for(int j = iCol; j >= 1; j--)
+            {
+                if((i == j) || (i == 1) || (j == 1) || (iRow == i) || (iCol == j))
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                  System.out.print("\t");  
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+public class Program16 
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int iRow = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        int iCol = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iRow, iCol);
+        
+        sobj.close();
+    }    
+}
