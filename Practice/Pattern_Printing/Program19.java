b5d6f8c1df885da3b59dec5edbdedd1670af0cd1
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-29 20:08:41 +0530

    Pattern Printing

---
+++ Practice/Pattern_Printing/Program19.java
@@ -0,0 +1,60 @@
+/*
+    iRow : 4
+    iCol : 4
+
+    1   2   3   4
+    5   6   7   8
+    9   1   2   3
+    4   5   6   7
+
+*/
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int iNo = 0;
+
+        iNo = 1;
+        for(int i = 1; i <= iRow; i++)
+        {
+            if(i % 2 == 0)
+            {
+                iNo =  1;
+            } 
+            else     
+            {
+                iNo = 2;
+            }  
+
+            for(int j = 1; j <= iCol; j++)
+            {
+                System.out.print(iNo + "\t");
+                iNo = iNo + 2;
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program19
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows");
+        int iRow = sobj.nextInt();
+
+        System.out.println("Enter the number of column");
+        int iCol = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iRow, iCol);
+
+        sobj.close();
+    }
+}
